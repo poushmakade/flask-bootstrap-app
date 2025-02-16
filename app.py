ebe830017fd3079a5f6559cf5cf1a51c82b86e99
Poush Makade <makadepm@rknec.edu>
2025-02-16 10:54:12 +0530

    changes

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')  # Render an HTML file
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0", port=8000) 
